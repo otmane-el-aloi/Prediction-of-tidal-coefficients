c21c57235618d1a26463cd720b99ca83bf11710a
EL ALOI Otmane <elaloi.otmane@gmail.com>
2021-11-20 21:57:50 +0100

    trained best RF model on custom features with 2 dim target variable

--- app.py
+++ app.py
@@ -13,31 +13,32 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-@app.route("/predict", methods=['POST'])
-def predict():
-    try:
-        test_json = request.get_json()
-        test = pd.read_json(test_json, orient='records')
-        test['dateTime'] = pd.to_datetime(test['dateTime'])
+# @app.route("/predict", methods=['POST'])
+# def predict():
+#     try:
+#         test_json = request.get_json()
+#         test = pd.read_json(test_json, orient='records')
+#         test['dateTime'] = pd.to_datetime(test['dateTime'])
 
-        # Getting previous coefficients
-        previous_coef = test["coef"].values
+#         # Getting previous coefficients
+#         previous_coef = test["coef"].values
     
-    except Exception as e:
-        raise e
+#     except Exception as e:
+#         raise e
 
-    # load model 
-    RF = RFModel()
-    RF.load("RF_model")
-    model = RF.model
+#     # load model 
+#     RF = RFModel()
+#     RF.load("RF_model")
+#     model = RF.model
 
-    # Making prediction 
-    predicted_future_coef = model.predict(previous_coef)
+#     # Making prediction 
+#     predicted_future_coef = model.predict(previous_coef)
 
-    # Transform to json
-    predicted_future_coef_df = pd.DataFrame(predicted_future_coef, columns = ["coef"])
-    response = jsonify(predictions=predicted_future_coef_df.to_json(orient="records"))
-    return response, 200
+#     # Transform to json
+#     predicted_future_coef_df = pd.DataFrame(predicted_future_coef, columns = ["coef"])
+#     response = jsonify(predictions=predicted_future_coef_df.to_json(orient="records"))
+#     return response, 200
+
 
 
 if __name__ == "__main__":
